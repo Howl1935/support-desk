b047b320f8daceee7088c49fd245186554bf1d18
Howl1935 <travisphawley@gmail.com>
2022-02-27 10:51:25 -0800

    Ready to deploy

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
+const noteRouter = require('./noteRoutes')
+
+// Re-route into note router
+router.use('/:ticketId/notes', noteRouter)
 const {getTickets, createTicket, getTicket, deleteTicket, updateTicket} = require('../controllers/ticketController')
 router.route('/').get(protect, getTickets).post(protect, createTicket);
 
